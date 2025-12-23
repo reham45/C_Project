24605eb8cd3ea5a21785d06963f29ec974fed2c1
Reham Abdelmoniem <rehamabdel45@gmail.com>
2025-12-23 17:42:31 +0200

    Update BIT.h

--- BIT.h
+++ BIT.h
@@ -1,9 +1,1 @@
-#ifndef _BIT_H_
-#define  _BIT_H_
 
-#define SET_BIT(var,bitno) (var|=(1<<bitno))
-#define CLR_BIT(var,bitno) (var&=(~(1<<bitno)))
-#define TOGGLE_BIT(var,bitno) (var^=(1<<bitno))
-#define GET_BIT(var,bitno) ((var>>bitno)&1)
-
-#endif
